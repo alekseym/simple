298b7ed45c525290e16571d38644fa5f2c8b77ce
Alex Maksimov <al_maksimov@mail.ru>
2013-11-16 22:15:15 +0400

    array.cpp was added

---
+++ array.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "array.h"
+
+
+// TO DO  Тут необходимо реализовать класс Array
+
+
+/*
+
+
+
+void init(){
+  printf("Init fuction has turned on \n");
+  for (int i=0; i<100; i++)
+    arr[i] = NULL;
+}
+
+void testfill()
+{
+  for (int i=0; i<100; i++)
+    arr[i] = new Simple ();
+}
+
+void showAll()
+{
+  for (int i = 0; i<100; i++)
+    arr[i]->show();
+}
+
+void deleteAll()
+{
+  for (int i=0; i<100; i++)
+    delete arr[i];
+}
+*/
