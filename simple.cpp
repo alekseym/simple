bd1a00d0172c9c8c6a41d1e24617367af5fb6a9f
Alex Maksimov <al_maksimov@mail.ru>
2013-11-16 12:10:50 +0400

    three debug checkpoints added to the program

--- simple.cpp
+++ simple.cpp
@@ -7,15 +7,21 @@ public:
 
 Simple(){
   val=0;
+  printf ("hello\n");
 }
 
-void show(){
+void show()
+{
   printf("%d \n",val);
 }
 };
 
 int main (){
+  printf("point1\n");
   Simple a;
+  Simple b;
+  Simple c;
+  printf("point2\n");
   a.show();
-
+  printf("point3\n");
 }
